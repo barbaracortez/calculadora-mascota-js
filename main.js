633d1c304eb24b8605251a8605066118f9ca67db
barbaracortez <cortezbarbara.dev@gmail.com>
2025-05-21 20:07:08 -0300

    Varsion 1 - saludo básico

---
+++ main.js
@@ -0,0 +1,16 @@
+//Pedimos el nombre del usuario
+let nombre = prompt("¿Cómo te llamas?");
+
+//Pedimos la edad de la mascota
+let edadMascota = prompt("¿Cuantos años tiene tu mascota");
+
+//Convertimos edad a número
+let edadHumana = edadMascota * 7;
+
+// Mostramos el resultado 
+alert("Hola "  +  nombre  + ", tu mascota tiene "+  edadHumana  + " años humanos. ");
+console.log("Resultado: " + edadHumana + "años humanos");
+
+
+
+
